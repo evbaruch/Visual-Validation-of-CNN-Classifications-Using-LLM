bcc97e1eb6c1e71a692d087b108e3fe9b80b6e87
aviaMader <100083767+aviaMader@users.noreply.github.com>
2024-11-07 15:09:34 +0200

    chatGpt local images using base64Imeges

--- chatGpt.py
+++ chatGpt.py
@@ -1,6 +1,17 @@
 from openai import OpenAI
+import base64
 
 client = OpenAI(api_key = "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")
+
+# Function to encode the image
+def encode_image(image_path):
+    with open(image_path, "rb") as image_file:
+        return base64.b64encode(image_file.read()).decode('utf-8')
+
+
+image_path = "images/Dog1.jpg"
+# Getting the base64 string
+base64_image = encode_image(image_path)
 
 completion = client.chat.completions.create(
     model="gpt-4o",
@@ -12,7 +23,7 @@ completion = client.chat.completions.create(
                 {
                     "type": "image_url",
                     "image_url": {
-                        "url": "https://upload.wikimedia.org/wikipedia/commons/thumb/d/dd/Gfp-wisconsin-madison-the-nature-boardwalk.jpg/2560px-Gfp-wisconsin-madison-the-nature-boardwalk.jpg",
+                        "url":  f"data:image/jpeg;base64,{base64_image}",
                     }
                 },
             ],
