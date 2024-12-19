37c19412961908ecab1da16e68bb01b1d404644e
evbaruch <evbaruch@g.jct.ac.il>
2024-12-19 18:29:09 +0200

    Add normalization to image transformation pipeline

--- data/global_data.py
+++ data/global_data.py
@@ -51,8 +51,9 @@ def initialize_transform():
     """
     return transforms.Compose([
         transforms.Resize((224, 224)),  # Resize images to 224x224
-        transforms.ToTensor()          # Convert images to tensors
-    ])
+        transforms.ToTensor(),          # Convert images to tensors
+        transforms.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225])  # Normalize images
+        ])
 
 def find_closest_category_idx(raw_label, imagenet_classes):
     """
