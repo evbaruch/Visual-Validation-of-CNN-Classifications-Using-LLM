8cbea9c27b5c2b98162ee83b77748135d6145f4f
aviaMader <100083767+aviaMader@users.noreply.github.com>
2025-01-24 07:55:00 +0200

    Refactor get_response method and add classification functionality with output type options

--- LmmApi/LLMInterface.py
+++ LmmApi/LLMInterface.py
@@ -58,10 +58,36 @@ class LLMInterface:
     def set_jsonDescription(self, jsonDescription: BaseModel):
         self.jsonDescription = jsonDescription
     
-    def get_response(self, image: str) -> str:
-        """Generate a response using the current strategy."""
-        return self.strategy.generate_response(self.prompt, image, self.jsonDescription)
+    # def get_response(self, image: str) -> str:
+    #     """Generate a response using the current strategy."""
+    #     return self.strategy.generate_response(self.prompt, image, self.jsonDescription)
          
+    def get_response(self, file_path: str , output_type: str = "None" ,category: str = "None") -> str:
+        """
+        Generate a response using the current strategy.
+
+        Args:
+            image (str): The path to the image file.
+            output_type (str, optional): The type of output desired. Options are 'structured', 'boolean', 'anchored'.
+                                        Defaults to 'structured'.
+
+        Returns:
+            str: The generated response.
+        """
+        
+        if output_type == "structured":
+            response = self.strategy.generate_response(self.prompt, file_path, self.jsonDescription)
+            return response
+        elif output_type == "boolean":
+            response = self.strategy.generate_response(f"What do you see in the picture? Is it a {category} from the imagenet database?", file_path, self.jsonDescription)
+            return "True" if response else "False"
+        elif output_type == "anchored":
+            response = self.strategy.generate_response(f"{self.prompt} Is this a {category}?", file_path, self.jsonDescription)
+            return f"Anchored response: {response}"
+        elif output_type == "None":
+           pass
+        else:
+            raise ValueError(f"Invalid output_type: {output_type}. Expected 'structured', 'boolean', or 'anchored'.")
     
     def process_response(self, response: list) -> str:
         """Process the response from the LLM."""
@@ -142,7 +168,7 @@ class LLMInterface:
                 
 
                 # Check if the true label is among the top 5 predicted labels
-                correctly = true_label in labels
+                correctly = true_label in labels # correctly is not the right name for this variable - rename to 'is_true_label_detected'
 
                 # Append the results to the matrix of responses
                 data.append([index, true_label, correctly] + labels + response_list)
@@ -151,7 +177,7 @@ class LLMInterface:
                 continue
 
             label_columns = [f'Class_{i+1}' for i in range(max_labels)]
-            llm_columns = [f'llm_{i+1}' for i in range(max_llm)]
+            llm_columns = [f'llm_{i+1}' for i in range(max_llm)] # llm_respondes_columns
             columns = ['Index', 'True_Label', 'Match'] + label_columns + llm_columns
             # Create a DataFrame from the array and add the column titles
             df = pd.DataFrame(data, columns=columns)
@@ -285,12 +311,57 @@ class LLMInterface:
 
             # Save the DataFrame to a CSV file
             df.to_csv(save_path_csv, index=False)
+            
+    def classification(self, root_directory: str, save_path: str, output_type: str, rewrite = False):
+        """
+        Classifies images in the specified root directory using a language model (LLM) strategy.
+        The classification results are saved in CSV files, with one CSV file per method (folder name).
 
-                
+        Parameters:
+        - root_directory (str): The root directory containing subdirectories with images to classify.
+        - save_path (str): The directory where the CSV files with classification results will be saved.
+        - output_type (str): The type of output desired. Options are 'structured', 'boolean', 'anchored'.
+        """
+        os.makedirs(save_path, exist_ok=True)
 
+        for method_dir, _, filenames in os.walk(root_directory): 
+            image_files = [img_file for img_file in filenames if img_file.endswith('.png')]
+            response_data = []
+            max_labels_count = 0
+            max_response_count = 0
 
+            method_name = os.path.basename(method_dir)
+            save_csv_path = os.path.join(save_path, f"{method_name}.csv")
+            if os.path.isfile(save_csv_path) and not rewrite:
+                continue
+        
+            for image_file in tqdm(image_files, desc=f"Processing files in {method_dir}"):
+                image_path = os.path.join(method_dir, image_file)
+                img_name = os.path.basename(image_path)
 
+                response = self.get_response(image_path, output_type ,img_name.split('_')[1].split('.')[0])
+                response_values = list(response.model_dump().values())
 
-                    
+                predicted_labels = []
+                for item in response_values:
+                    _, closest_label = gd.find_closest_category(item, self.imagenet_categories)
+                    predicted_labels.append(closest_label)
 
+                max_labels_count = max(max_labels_count, len(predicted_labels))
+                max_response_count = max(max_response_count, len(response_values))
 
+                index, label_with_extension = image_file.split('_')[:2]
+                true_label = label_with_extension.split('.')[0]
+
+                label_match = true_label in predicted_labels
+
+                response_data.append([index, true_label, label_match] + predicted_labels + response_values)
+
+            if not response_data:
+                continue
+
+            label_columns = [f'Class_{i+1}' for i in range(max_labels_count)]
+            response_columns = [f'llm_{i+1}' for i in range(max_response_count)]
+            columns = ['Index', 'True_Label', 'Match'] + label_columns + response_columns
+            df = pd.DataFrame(response_data, columns=columns)
+            df.to_csv(save_csv_path, index=False)
