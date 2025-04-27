aea56e14565fc63037871eaa73f472708d3ab99e
rosenfa <rosenfa@gmail.com>
2025-04-27 13:19:30 +0300

    Update accuracy calculation logic and result files for Random and Saliency methods; adjust percentage ranges and add missing data points.

--- test2.py
+++ test2.py
@@ -2,7 +2,7 @@ from data import dynamic_results as dr
 from data import results
 
 dr.getDynamicResults("Saliency", "boolean", "llm_answer_precentage", "midPrecentage")
-dr.add_precentage_to_csv("Random", "boolean", "llm_answer_precentage", "midPrecentage")
+dr.getDynamicResults("Random", "boolean", "llm_answer_precentage", "midPrecentage")
 
 results.calculate_accuracy("data\\llm_answer_precentage\\Random\\boolean","data\\llm_answer_precentage\\Random\\boolean\\results")
 results.calculate_accuracy("data\\llm_answer_precentage\\Saliency\\boolean","data\\llm_answer_precentage\\Saliency\\boolean\\results")
