72c8abaec3f45d28e4c64ed9dec956e963cdd4f3
yehudajct <yehuda.game@gmail.com>
2025-04-28 12:14:43 +0300

    Add results and pivot tables for Random, Saliency, GuidedGradCam, and InputXGradient methods; update .gitignore and test4.py for new result paths

--- test4.py
+++ test4.py
@@ -7,8 +7,11 @@ if __name__ == "__main__":
     # dr.getDynamicResults("GradientShap", "boolean", "llm_answer_precentage", "midPrecentage")
     # dr.getDynamicResults("Random", "boolean", "llm_answer_precentage", "midPrecentage")
 
-    results.calculate_accuracy("data\\llm_answer_precentage\\InputXGradient\\boolean","data\\llm_answer_precentage\\InputXGradient\\boolean\\results")
-    results.calculate_accuracy("data\\llm_answer_precentage\\GradientShap\\boolean","data\\llm_answer_precentage\\GradientShap\\boolean\\results")
-    results.calculate_accuracy("data\\llm_answer_precentage\\GuidedGradCam\\boolean","data\\llm_answer_precentage\\GuidedGradCam\\boolean\\results")
+    # results.calculate_accuracy("data\\llm_answer_precentage\\InputXGradient\\boolean","data\\llm_answer_precentage\\InputXGradient\\boolean\\results")
+    # results.calculate_accuracy("data\\llm_answer_precentage\\GradientShap\\boolean","data\\llm_answer_precentage\\GradientShap\\boolean\\results")
+    # results.calculate_accuracy("data\\llm_answer_precentage\\GuidedGradCam\\boolean","data\\llm_answer_precentage\\GuidedGradCam\\boolean\\results")
     results.calculate_accuracy("data\\llm_answer_CervicalCancer_COMPLETE\\Random\\boolean","data\\llm_answer_CervicalCancer_COMPLETE\\Random\\boolean\\results")
     results.calculate_accuracy("data\\llm_answer_CervicalCancer_COMPLETE\\Saliency\\boolean","data\\llm_answer_CervicalCancer_COMPLETE\\Saliency\\boolean\\results")
+    results.calculate_accuracy("data\\llm_answer_CervicalCancer_COMPLETE\\GuidedGradCam\\boolean","data\\llm_answer_CervicalCancer_COMPLETE\\GuidedGradCam\\boolean\\results")
+    results.calculate_accuracy("data\\llm_answer_CervicalCancer_COMPLETE\\InputXGradient\\boolean","data\\llm_answer_CervicalCancer_COMPLETE\\InputXGradient\\boolean\\results")
+
